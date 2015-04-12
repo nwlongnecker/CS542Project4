f699f318aa42e237c105bfee5a84cb385c750ca3
Nathan Longnecker <nathanlongnecker@gmail.com>
2015-04-12 16:50:47 -0400

    Add update operation

---
+++ src/relation/UpdateOperation.java
@@ -0,0 +1,58 @@
+package relation;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Executes an update operation on a relation based on a specified conditional. The output
+ * is written to a buffer as GetNext() is called, eventually sending EOF with Close().
+ */
+public class UpdateOperation extends Operation {
+	private List<Integer> compareOn;
+	private Conditional<String> conditional;
+	private List<Integer> updateIndices;
+	private List<String> newValues;
+
+	public UpdateOperation(Reader in, Writer out, List<Integer> compareOn, Conditional<String> conditional, List<Integer> updateIndices, List<String> newValues) {
+		super(in, out);
+		this.compareOn = compareOn;
+		this.conditional = conditional;
+		this.updateIndices = updateIndices;
+		this.newValues = newValues;
+	}
+
+	@Override
+	public void getNext() throws IOException {
+		String line = null;
+
+		while ((line = readNextLine()) != null) {
+			// use comma as separator
+			String[] tuple = line.split(SEPARATOR);
+			
+			List<String> attributes = new ArrayList<String>();
+			for(Integer i : compareOn) {
+				attributes.add(tuple[i]);
+			}
+			try {
+				if (conditional.compare(attributes)) {
+					for (int i = 0; i < updateIndices.size(); i++) {
+						tuple[updateIndices.get(i)] = newValues.get(i);
+					}
+					StringBuilder builder = new StringBuilder("");
+					for (int i = 0; i < tuple.length-1; i++) {
+						builder.append(tuple[i]);
+						builder.append(SEPARATOR);
+					}
+					builder.append(tuple[tuple.length-1]);
+					builder.append('\n');
+					out.write(builder.toString());
+				} else {
+					out.write(line+'\n');
+				}
+			} catch (Exception e) {e.printStackTrace();}
+		}
+	}
+}
--- test/relation/OperationTest.java
+++ test/relation/OperationTest.java
@@ -45,7 +45,7 @@ public class OperationTest {
 		
 		List<Integer> compareOn = new ArrayList<Integer>();
 		compareOn.add(4);
-		Operation select = new SelectOperation(in, out, compareOn, value -> Integer.parseInt(value.get(0)) > 500000);
+		Operation select = new SelectOperation(in, out, compareOn, values -> Integer.parseInt(values.get(0)) > 500000);
 		
 		select.open();
 		select.getNext();
@@ -67,12 +67,34 @@ public class OperationTest {
 		attributesOne.add(0);
 		List<Integer> attributesTwo = new ArrayList<Integer>();
 		attributesTwo.add(2);
-		Operation join = new JoinOperation(in, in2, out, attributesOne, attributesTwo, (value1, value2) -> value1.get(0).equals(value2.get(0)));
+		Operation join = new JoinOperation(in, in2, out, attributesOne, attributesTwo, (values1, values2) -> values1.get(0).equals(values2.get(0)));
 		
 		join.open();
 		join.getNext();
 		join.close();
 		
 		assertEquals("MA,30000000,US,Boston,12345,1000000\nMA,30000000,US,Worcester,01609,750000\n", output.toString());
+	}
+	
+	@Test
+	public void testUpdate() throws Exception {
+		String input = "US,Boston,MA,12345,1000000\nUS,Lincoln,MA,01773,15000\nUS,Worcester,MA,01609,750000";
+		Reader in = new InputStreamReader(new ByteArrayInputStream(input.getBytes()));
+		OutputStream output = new ByteArrayOutputStream();
+		Writer out = new OutputStreamWriter(output);
+		
+		List<Integer> compareOn = new ArrayList<Integer>();
+		compareOn.add(1);
+		List<Integer> updateIndices = new ArrayList<Integer>();
+		updateIndices.add(4);
+		List<String> newValues = new ArrayList<String>();
+		newValues.add("755000");
+		Operation update = new UpdateOperation(in, out, compareOn, values -> values.get(0).equals("Worcester"), updateIndices, newValues);
+		
+		update.open();
+		update.getNext();
+		update.close();
+		
+		assertEquals("US,Boston,MA,12345,1000000\nUS,Lincoln,MA,01773,15000\nUS,Worcester,MA,01609,755000\n", output.toString());
 	}
 }
