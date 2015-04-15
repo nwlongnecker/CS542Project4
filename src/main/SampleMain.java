eeb037024752a5dea31650de161486ea4bee098a
Mike Calder <mrcalder@wpi.edu>
2015-04-15 18:17:18 -0400

    Updated SampleMain and Generalized DB Sync

--- src/main/SampleMain.java
+++ src/main/SampleMain.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,6 +17,16 @@ public class SampleMain {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
+		// Make directories for the two databases.
+		new File("sampleDatabase1").mkdir();
+		new File("sampleDatabase2").mkdir();
+		
+		// Copy the original relation files into both databases.
+		Files.copy(Paths.get("originalData/country.csv"), Paths.get("sampleDatabase1/country.csv"));
+		Files.copy(Paths.get("originalData/city.csv"), Paths.get("sampleDatabase1/city.csv"));
+		Files.copy(Paths.get("originalData/country.csv"), Paths.get("sampleDatabase2/country.csv"));
+		Files.copy(Paths.get("originalData/city.csv"), Paths.get("sampleDatabase2/city.csv"));
+		
 		// Track the indices we want to update in the countries relation.
 		List<Integer> countryIndices = new LinkedList<Integer>();
 		countryIndices.add(6);
@@ -23,18 +36,23 @@ public class SampleMain {
 		cityIndices.add(4);
 		
 		// Create a new Database object and update the relations as requested in the project spec.
-		Database db = new Database("sampleDatabase");
+		Database db1 = new Database("sampleDatabase1");
 		// Begin a transaction.
-		db.startTransaction(1);
+		db1.startTransaction(1);
 		// Multiply every population in the countries relation by 1.02 (increase by 2%).
-		db.update(1, "country", countryIndices, (value) -> true, countryIndices, (value) -> "\"" +
+		db1.update(1, "country", countryIndices, (value) -> true, countryIndices, (value) -> "\"" +
 				Integer.toString((int)(Integer.parseInt(value.get(0).replace("\"", "")) * 1.02)) + "\"");
 		// Multiply every population in the countries relation by 1.02 (increase by 2%).
-		db.update(1, "city", cityIndices, (value) -> true, cityIndices, (value) -> "\"" +
+		db1.update(1, "city", cityIndices, (value) -> true, cityIndices, (value) -> "\"" +
 				Integer.toString((int)(Integer.parseInt(value.get(0).replace("\"", "")) * 1.02)) + "\"");
 		// Commit the transaciton.
-		db.commit(1);
+		db1.commit(1);
+		
+		// Create a new Database object and sync it with the first database using its undo/redo log.
+		Database db2 = new Database("sampleDatabase2");
+		// Copy the log file from database1 to database2.
+		Files.copy(Paths.get("sampleDatabase1/db.log"), Paths.get("sampleDatabase2/db.log"));
 		// This method can be used at any time to sync with the database log.
-		db.syncWithLog();
+		db2.syncWithLog();
 	}
 }
